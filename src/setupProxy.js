9029338e504d1bd216f82e4ad4f87e69124d3d50
Sunflower Game Art Studio <sunflowergameartstudio@gmail.com>
2022-07-11 19:43:21 -0300

    Tudo

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(createProxyMiddleware('/api/', // replace with your endpoint
-        { target: 'http://localhost:8000' } // replace with your target
+        { target: 'https://howxfront.herokuapp.com' } // replace with your target
     ));
 }
