4398924fe3721a9e018a9f166c4bac00514ea320
Sunflower Game Art Studio <sunflowergameartstudio@gmail.com>
2022-07-11 21:06:32 -0300

    Up

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,3 +5,12 @@ module.exports = function(app) {
         { target: 'https://howxfront.herokuapp.com' } // replace with your target
     ));
 }
+
+const trajectory = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(trajectory, "/dist")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(trajectory, "dist", "index.html"))
+    );
+}
--- vue.config.js
+++ vue.config.js
@@ -10,3 +10,18 @@
 //  proxy: '*',
 //  }
 //}
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
+}
+
+const trajectory = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(trajectory, "/dist")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(trajectory, "dist", "index.html"))
+    );
+}
